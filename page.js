4045769ba8fcbc195df20bea5d624bb037e65f15
anhthy95 <anhthy95@gmail.com>
2019-12-12 19:18:45 -0700

    nav bar fix

--- page.js
+++ page.js
@@ -2,6 +2,8 @@
 function fixBar(){
 var sidebar = document.getElementById("sidebar");
 let myElement = document.querySelector("#grid-container");
+console.log("run");
+console.log(screen.width);
 
 if(screen.width < "700px"){
    sidebar.className= 'w3-sidebar w3-bar-block w3-black w3-large'; // += ' blueClass'; to keep existing classes
@@ -9,12 +11,15 @@ if(screen.width < "700px"){
    myElement.style.gridTemplateColumns = "30px 1fr";
 
 }
+
 else if (screen.width < "900px") {
    sidebar.className= 'w3-sidebar w3-bar-block w3-black w3-xlarge'; // += ' blueClass'; to keep existing classes
    sidebar.style.width="50px"; 
    myElement.style.gridTemplateColumns = "50px 1fr";
 }
 }
+
+
 
   $( function() {
     $( "#datepicker" ).datepicker();
